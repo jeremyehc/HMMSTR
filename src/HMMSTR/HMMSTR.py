5783d0a7f8b06b2448fc884f8be7f0d1ad87dbeb
jeremyehc <varscrest@gmail.com>
2024-04-15 13:56:05 -0400

    testing

--- src/HMMSTR/HMMSTR.py
+++ src/HMMSTR/HMMSTR.py
@@ -566,6 +566,7 @@ def call_peaks_stranded(row, out, out_count_name, plot_hists, max_peaks, filter_
 
 
 def main():
+    print("Running new model")
     parser = argparse.ArgumentParser()
     #Required inputs
     subparsers = parser.add_subparsers(dest='subcommand')
